b56a3c077373a69cd8b671bc8067f149972da1da
MarcoChavez1940 <marcostarr1940@gmail.com>
2018-05-10 21:48:36 -0500

    done general chat at lobby

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,11 +1,16 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+
+import * as socketIo from "socket.io-client";
 
 @Component({
   selector: "app-chat",
   templateUrl: "./chat.component.html",
   styleUrls: ["./chat.component.css"]
 })
-export class ChatComponent {
+export class ChatComponent implements OnInit{
+
+  public socket: any;
+
   private messages: Message[] = [
     {
       username: "WillyRex",
@@ -17,13 +22,31 @@ export class ChatComponent {
     }
   ];
 
-  constructor() {}
+  constructor() {
+    this.socket = socketIo('http://localhost:3000');
+  }
+
+  ngOnInit(): void {
+    this.socket.on('new Message General', (message) =>{
+      var newMessage: Message = {
+        username: message.username,
+        content: message.content  
+      }
+
+      this.messages.push(newMessage);
+      setTimeout(this.scrollToBottom, 10);
+    })
+
+    
+  }
 
   sendMessage(username: string, content: HTMLInputElement) {
     var newMessage: Message = {
       username: username,
       content: content.value
     };
+
+    this.socket.emit('new Message General', newMessage);
 
     this.messages.push(newMessage);
     content.value = null;
