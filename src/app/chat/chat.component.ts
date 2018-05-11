88349c8776afcdd2e35b00cf5a8f52bf41b2e273
MarcoChavez1940 <marcostarr1940@gmail.com>
2018-05-11 03:59:16 -0500

    done partially chats user to user

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -24,19 +24,19 @@ export class ChatComponent implements OnInit{
     }
   ];
 
-  private idChatSelected: number = 1;
+  private idChatSelected: number = 0;
 
   private globalChat: Chat = {
-    id_chat: 1,
+    id_chat: 0,
     user_contact: 'Global',
     messages: [
       {
-        id_chat: 1,
+        id_chat: 0,
         username: "MLGPro",
         content: "Retas putos"
       },
       {
-        id_chat: 1,
+        id_chat: 0,
         username: "ChinoAsiticoJapones",
         content: "ora ora ora ora!"
       }
@@ -51,25 +51,49 @@ export class ChatComponent implements OnInit{
 
   constructor() {
     this.socket = socketIo('http://localhost:3002');
-    this.messages = this.chats.find(function (chat) { return chat.id_chat === 1}).messages;
+    this.messages = this.chats.find(function (chat) { return chat.id_chat === 0}).messages;
   }
 
   ngOnInit(): void {
-    this.socket.on('new Message', (message) =>{
+    this.socket.on('new_message_general', (message) =>{
       var newMessage: Message = {
         id_chat: message.id_chat,
         username: message.username,
         content: message.content  
       }
 
-      var chat = this.chats.find(function (chat) { return chat.id_chat === message.id_chat});
-      
-      if(chat !== undefined){
-        chat.messages.push(newMessage);
-      }
+      this.chats[0].messages.push(newMessage);
 
       setTimeout(this.scrollToBottom, 10);
     })
+    
+    this.socket.on('init_chat_Braulio' /* + current_user */, (chat) =>{
+
+      let remoteChat: Chat = {
+        id_chat: chat.id_chat,
+        user_contact: chat.user_sender,
+        messages:  chat.messages
+      };
+     
+      this.chats.push(remoteChat);
+      
+      this.socket.emit('entry_chat', chat.id_chat);
+
+    });
+
+    this.socket.on('new_message_particular', (message) =>{
+      var newMessage: Message = {
+        id_chat: message.id_chat,
+        username: message.username,
+        content: message.content  
+      }
+      //Si el user ya cerro su chat, marcara un error al querer buscar el chat para
+      //añadir el chat
+      var chat = this.chats.find(function (chat) { return chat.id_chat === message.id_chat})
+      .messages.push(newMessage);
+
+      setTimeout(this.scrollToBottom, 10);
+    });
 
     
   }
@@ -81,13 +105,14 @@ export class ChatComponent implements OnInit{
       content: content.value
     };
 
-    this.socket.emit('new Message', newMessage);
-
+    if(id_chat === 0){
+      this.socket.emit('new_message_general', newMessage); 
+    }else{
+      this.socket.emit('new_message_particular', newMessage);
+    }
+    
     var chat = this.chats.find(function (chat) { return chat.id_chat === id_chat}).messages.push(newMessage);
 
-
-
-    //this.messages.push(newMessage);
     content.value = null;
     setTimeout(this.scrollToBottom, 10);
   }
@@ -99,24 +124,28 @@ export class ChatComponent implements OnInit{
   }
 
   addChat(username: string, id_chat_correspondent: number){
-    let chat: Chat = {
+    let chat = {
       id_chat: id_chat_correspondent,
+      user_sender: 'Marco', /* current User */
       user_contact: username,
-      messages: [
-        {
-          id_chat: id_chat_correspondent,
-          username: "CharlieUwU",
-          content: "Te amo <3"
-        },
-        {
-          id_chat: id_chat_correspondent,
-          username: "Marco",
-          content: "Yo mas bb :*"
-        }
-      ]
+      messages: []
     };  
+    
+
+    this.socket.emit('init_chat', chat);
 
     this.chats.push(chat);
+  }
+
+  removeChat(id_chat: number){
+    this.chats = this.chats.filter(function( chat ) {
+      return chat.id_chat !== id_chat;
+    });
+    
+    this.changeChat(0);
+
+    this.socket.emit('close_chat', this.idChatSelected);
+
   }
 
 
